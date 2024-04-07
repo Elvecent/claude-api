899c813c4cb8e8c80e78ff3a498b6209d1cc0e15
Kirill Valiavin <kvalyavin@ya.ru>
2024-04-07 10:49:24 +0700

    improve nix stuff

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { nixpkgs ? <nixpkgs>, compiler ? "ghc948", withHoogle ? false}:
 
 let
-  claude-api = pkgs.haskellPackages.callCabal2nix "claude-api" ./. {};
+  claude-api = import ./. { inherit pkgs; };
   overlay = self: super: {
     haskellPackages = pkgs.haskell.packages.${compiler}.override (old: {
       overrides = self: super: {
@@ -11,9 +11,8 @@ let
   pkgs = import nixpkgs { overlays = [ overlay ]; };
   shellLocal = if builtins.pathExists ./shell-local.nix then import ./shell-local.nix else {};
 in
-pkgs.haskellPackages.shellFor {
+pkgs.haskellPackages.shellFor ({
   inherit withHoogle;
   packages = _: [ claude-api ];
   buildInputs = with pkgs.haskellPackages; [ haskell-language-server pkgs.nodejs ];
-  inherit (shellLocal) shellHook;
-}
+} // shellLocal)
